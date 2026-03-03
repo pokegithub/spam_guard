3972dd0ce61a9d7a1ae3c8c8fe1ac9a4e6f5d03e
Shrijit Mittra <78595914+shrijitMittra08@users.noreply.github.com>
2026-03-03 17:44:40 +0530

    Updated to not delete label column in testing data
    Add functionality to save testing data to a CSV file.

--- dataset_split.py
+++ dataset_split.py
@@ -7,11 +7,11 @@ df = pd.read_csv('data.csv')
 #df_testing = df.drop(df_training.index)
 
 df_training, df_testing = train_test_split(df, test_size = 0.3, random_state = 36)
-df_testing = df_testing.drop(columns=['label'])
 
 print(df_training['label'].value_counts())
 
 #df_testing = df_testing.drop(columns = ['label'])
 
 df_training.to_csv('training_data.csv', index = False)
+
 df_testing.to_csv('testing_data.csv', index = False)
